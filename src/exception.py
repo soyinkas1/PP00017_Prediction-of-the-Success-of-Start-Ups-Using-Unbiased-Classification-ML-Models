47a0935857cccaa36671d654de4087e56766c2ba
unknown <soyinkas1@gmail.com>
2023-12-10 00:03:19 +0000

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 
 def error_message_detail(error, error_detail:sys):
@@ -12,11 +13,9 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
 
     def __str__(self):
         return self.error_message
-
-
