a1db4278846dbb21a3e5d3920de2330f1e37aeb5
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-02-16 20:51:55 +0000

    university ranking scrapper script added

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,53 +1,54 @@
 # Import the required libraries
 import pandas as pd
 import os
-import sys
-from src.exception import CustomException
-from src.logger import logging
-from src.entity.config_entity import DataCleaningConfig, 
+from datetime import date
+# import sys
+# from src.exception import CustomException
+# from src.logger import logging
+# from src.entity.config_entity import DataCleaningConfig, 
 
-# Import Data Cleaning and Wrangling Libraries
-import pandas as pd
-import numpy as np
-import re
-import geopandas as gpd
-import ydata_profiling
+# # Import Data Cleaning and Wrangling Libraries
+# import pandas as pd
+# import numpy as np
+# import re
+# import geopandas as gpd
+# import ydata_profiling
 
-# Import Visualisation Libraries
-import seaborn as sns
-import matplotlib.pyplot as plt
-%matplotlib inline
-import plotly.express as px
-
-
-# Import NLP Libraries
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-
-# Import Machine Learning Classifiers models Libraries
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import MultinomialNB
-
-# Import evaluations modules
-from sklearn.metrics import plot_roc_curve
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.metrics import precision_score, recall_score, f1_score
-from sklearn.metrics import RocCurveDisplay
+# # Import Visualisation Libraries
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+# import plotly.express as px
 
 
-# Import other needed achine Learning Libraries
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder,MinMaxScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_val_score
-from sklearn.feature_selection import SelectPercentile, chi2
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import MultinomialNB
+# # Import NLP Libraries
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.feature_extraction.text import CountVectorizer
+
+# # Import Machine Learning Classifiers models Libraries
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.svm import SVC
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.naive_bayes import MultinomialNB
+
+# # Import evaluations modules
+# from sklearn.metrics import plot_roc_curve
+# from sklearn.metrics import confusion_matrix, classification_report
+# from sklearn.metrics import precision_score, recall_score, f1_score
+# from sklearn.metrics import RocCurveDisplay
+
+
+# # Import other needed achine Learning Libraries
+# from sklearn.compose import ColumnTransformer
+# from sklearn.pipeline import Pipeline
+# from sklearn.impute import SimpleImputer
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder,MinMaxScaler
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_val_score
+# from sklearn.feature_selection import SelectPercentile, chi2
+# from sklearn.svm import SVC
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.naive_bayes import MultinomialNB
 
 
 
@@ -77,5 +78,4 @@ class DataTransformation:
     # completed_on
     # started_on
     # Finally we will rearrange the features as Index -> Numerical -> Categorical -> Text -> Target/Label
-    
     
