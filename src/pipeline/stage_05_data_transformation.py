c15b451ffd2799e350de5e6ad583d8aca2f55347
Soyinka Sowoolu <soyinkas1@gmail.com>
2024-07-04 20:39:32 +0100

    load data from azure

--- src/pipeline/stage_05_data_transformation.py
+++ src/pipeline/stage_05_data_transformation.py
@@ -1,3 +1,4 @@
+from src.components import data_ingestion
 from src.config.configuration import ConfigurationManager
 from src.components.data_transformation import DataTransformation
 from src.logger import logging
@@ -17,7 +18,8 @@ class DataTransformationPipeline:
         config = ConfigurationManager()
         data_transform_config = config.get_data_transform_config()
         data_cleaning_config = config.get_data_cleaning_config()
-        data_transform = DataTransformation(config=data_transform_config, clean_data_config=data_cleaning_config)
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_transform = DataTransformation(config=data_transform_config, clean_data_config=data_cleaning_config, ingestion_config=data_ingestion_config)
         data_transform.data_transformation()
 
 
